1c49f1396c1d3d5500e51ef31bc60905f59a13a8
Jihun <110877415+hjh3229@users.noreply.github.com>
2023-06-05 14:16:00 +0900

    Update CreateMemo.java

--- team-project-memoList/src/memoList/CreateMemo.java
+++ team-project-memoList/src/memoList/CreateMemo.java
@@ -1,23 +1,18 @@
 package memoList;
 
-import java.util.ArrayList;
+public class ReadMemo implements MemoListManager{
 
-public class CreateMemo implements  MemoListManager{
     // <====================== 확인 후 채택 여부 결정 ======================> //
-    // <====================== 이 부분은 임시로 작성함 ======================> //
-    MemoList createMemolist;
+    MemoList readMemoList;
 
-    public CreateMemo(MemoList memoList) {
-        this.createMemolist = memoList;
+    public ReadMemo(MemoList memoList) {
+        this.readMemoList = memoList;
     }
 
     @Override
     public void performAction() {
-        System.out.println("메모를 추가 테스트");
-
-        ArrayList<Memo>memos = createMemolist.getMemos();
-        memos.add(new Memo(1,"오세창", "1234","안녕하세요", "06.04"));
-
-
+        for (Memo memo : readMemoList.getMemos()) {
+            System.out.println(memo.toString(memo.getMemoNumber(), memo.getWriterName(), memo.getWritedMemo(), memo.getWritedTime()));
+        }
     }
 }
