70f7d4a2f02d32c3d09f819a165eefcc8513ea86
Jihun <110877415+hjh3229@users.noreply.github.com>
2023-06-05 14:18:53 +0900

    Update Memo.java

--- team-project-memoList/src/memoList/Memo.java
+++ team-project-memoList/src/memoList/Memo.java
@@ -11,9 +11,6 @@ public class Memo {
     private String writedMemo; // 글 내용 // 한지훈 : 제가 기억하기 편한 변수명으로 수정했습니다.
     private String writedTime; // 작성일 (컴퓨터 시스템의 날짜, 시간을 자동으로 저장)
 
-    // getter/setter는 완성 후 사용하지 않는 것을 지워주세요
-
-
     public int getMemoNumber() {
         return memoNumber;
     }
@@ -26,16 +23,8 @@ public class Memo {
         return writerName;
     }
 
-    public void setWriterName(String writerName) {
-        this.writerName = writerName;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getWritedMemo() {
@@ -48,10 +37,6 @@ public class Memo {
 
     public String getWritedTime() {
         return writedTime;
-    }
-
-    public void setWritedTime(String writedTime) {
-        this.writedTime = writedTime;
     }
 
     // 메모 내용 백업 (작성자 : 오세창 / 수정 취소 기능 구현을 해보려고, 한번 생성해봤습니다,,,)
@@ -71,18 +56,12 @@ public class Memo {
     public String toString(int memoNumber, String writerName, String writedMemo, String writedTime) { // 비밀번호는 출력 X
         return String.format("%d.  %s \t: %s \t\t %s",memoNumber, writerName, writedMemo, writedTime); // 1.  한지훈   : 안녕하세요         2023/06/03 14:56:27
     }
-
-    /*
-     * 생성 시 현재 시간을 저장하도록 해둠
-     * 입력 시 저장하는 것으로 변경하고 싶으시면 아래 코드 복사해서 입력 시 값 넣어주세요!
-     */
-
+    
     public void editWtritedTime(){ // 기본 생성자는 변수들을 입력받는데 사용하고 싶어 메서드명을 수정했습니다.
         LocalDateTime now = LocalDateTime.now();  // 현재 시간
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         // 포맷 정의 포맷은 연/월/일 시간:분:초 >> 변경 가능
         //ex) 2023/06/03 13:42:10
         this.writedTime = now.format(formatter); // 포맷 적용, 변수 저장
-        System.out.println(writedTime); //테스트용 출력 / 추후 삭제 해주세요
     }
 }
